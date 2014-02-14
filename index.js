696644614050646ba8c39c89102370a6ed9adbd2
Obaid Rizavi <obaid.rizavi@gmail.com>
2014-02-14 20:03:48 +0500

    Allow index.js to accept user defined port param

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 var server = require("./server");
+var defaultPort = 8888;
+var port = defaultPort;
 
-server.start(8888);
+process.argv.forEach(function (val, index, array) {
+	if (index == '02') {
+		port = val;
+	}
+});
+
+server.start(port);
