ddb0b27367c65ffe38ea0b980645137e85e3cc55
Obaid Rizavi <obaid.rizavi@gmail.com>
2014-02-14 19:55:09 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 var http = require("http");
-var port = 8888;
+var url = require("url");
+var querystring = require("querystring");
+var router = require("./router");
 
-console.log("Starting simple HTTP server using Node.js");
+function start (port) {
+	console.log("Starting simple HTTP server using Node.js");
+	var onRequest = function(request, response) {
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request received ");
+		router.route(pathname);
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.write("Hello World");
+		response.write("\nHTTP Server started at port: " + port);
+		response.end();
+	}
+	var server = http.createServer(onRequest).listen(port);
 
-var onRequest = function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
+	console.log("Simple HTTP server now running on port: " + port);
 }
-var server = http.createServer(onRequest).listen(port);
 
-console.log("Simple HTTP server now running on port: " + port);
+exports.start = start;
